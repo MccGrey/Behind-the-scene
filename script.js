477a5516d155ff183d39c2949d32c34b57931a4a
MccGrey <chuxzzie01@yahoo.com>
2022-09-10 09:19:03 +0100

    scoping

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+
+  function printAge() {
+    const output = `My name is ${firstName} and I am ${age} years old, born in ${birthYear}`;
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `oh, you are a millenial, ${firstName}`;
+      console.log(str);
+    }
+
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'John';
+calcAge(1991);
