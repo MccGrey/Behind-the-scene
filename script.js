6205c2135bd18ad2426c71a7438100c85cba6d00
MccGrey <chuxzzie01@yahoo.com>
2022-09-10 09:47:48 +0100

    scoping

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ function calcAge(birthYear) {
     const output = `My name is ${firstName} and I am ${age} years old, born in ${birthYear}`;
 
     if (birthYear >= 1981 && birthYear <= 1996) {
+      const firstName = 'drake';
       const str = `oh, you are a millenial, ${firstName}`;
       console.log(str);
-    }
 
-    console.log(output);
+      function add(a, b) {
+        return a + b;
+      }
+      console.log(add(2, 3));
+    }
   }
   printAge();
   return age;
