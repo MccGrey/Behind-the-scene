9b633e3643465b9a9ba986309a5e7d3ad0eddc82
MccGrey <chuxzzie01@yahoo.com>
2022-09-12 05:54:05 +0100

    tasks: this keyword

--- script.js
+++ script.js
@@ -56,24 +56,48 @@
 // const z = 3;
 
 //THIS KEYWORD
-console.log(this);
+// console.log(this);
 
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAge(1991);
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAge(1991);
 
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAgeArrow(1991);
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAgeArrow(1991);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//   },
+// };
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
 
 const jonas = {
+  firstName: 'jonas',
   year: 1991,
   calcAge: function () {
     console.log(this);
+    console.log(2037 - this.year);
+
+    const isMillenial = function () {
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
   },
+
+  greet: () => console.log(`hey ${this.firstName}`),
 };
+jonas.greet();
 jonas.calcAge();
